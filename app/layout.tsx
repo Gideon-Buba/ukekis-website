0e0d83dde9f4320b0ea86461127e5481b9f45393
Gideon-Buba <bubaambore@gmail.com>
2026-01-12 08:37:30 +0100

    Add metadata to RootLayout and include new SVG assets for favicon and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import "./globals.css";
 import Footer from "./components/Footer";
 
+export const metadata = {
+  title: "UKEKI'S",
+  description: "Your business is just a click away from more clients",
+  icons: {
+    icon: "/ukeki_logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
